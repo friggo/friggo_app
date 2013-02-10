02789c1e670ff3a7834602fc4b122b98ece4332f
Eyal <bar_eyal@hotmail.com>
2013-02-10 23:06:54 +0200

    Add Application Helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,9 +11,14 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "#{base_title} | Home")
+      page.should have_selector('title', :text => "#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
@@ -28,6 +33,11 @@ describe "Static pages" do
       visit '/static_pages/help'
       page.should have_selector('title', :text => "#{base_title} | Help")
     end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Help')
+    end
   end
 
   describe "About page" do
@@ -41,6 +51,11 @@ describe "Static pages" do
       visit '/static_pages/about'
       page.should have_selector('title', :text => "#{base_title} | About Us")
     end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| About Us')
+    end    
   end
 
   describe "Contact page" do
@@ -54,5 +69,10 @@ describe "Static pages" do
       visit '/static_pages/contact'
       page.should have_selector('title', :text => "#{base_title} | Contact")
     end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Contact')
+    end        
   end
 end
